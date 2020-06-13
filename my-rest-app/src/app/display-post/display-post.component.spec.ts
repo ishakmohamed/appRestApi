b6358037fc1fa836fa6836a09525667a41952ef8
Mohamed Ishak <mohamed.ishak@cgi.com>
2020-06-13 16:01:11 -0400

    add unit test

--- my-rest-app/src/app/display-post/display-post.component.spec.ts
+++ my-rest-app/src/app/display-post/display-post.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DisplayPostComponent } from './display-post.component';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('DisplayPostComponent', () => {
   let component: DisplayPostComponent;
@@ -8,7 +10,9 @@ describe('DisplayPostComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DisplayPostComponent ]
+      declarations: [ DisplayPostComponent ],
+      providers: [HttpClient, HttpHandler],
+      imports: [RouterTestingModule],
     })
     .compileComponents();
   }));
--- my-rest-app/src/app/post-detail/post-detail.component.spec.ts
+++ my-rest-app/src/app/post-detail/post-detail.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PostDetailComponent } from './post-detail.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { FormControl } from '@angular/forms';
 
 describe('PostDetailComponent', () => {
   let component: PostDetailComponent;
@@ -8,7 +11,9 @@ describe('PostDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PostDetailComponent ]
+      declarations: [ PostDetailComponent ],
+      providers: [HttpClient, HttpHandler],
+      imports: [RouterTestingModule],
     })
     .compileComponents();
   }));
@@ -22,4 +27,26 @@ describe('PostDetailComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('user name is required', () => {
+    const  formControlPost = component.postGroupDetails;
+    expect(formControlPost.get('userId').valid).toEqual(false);
+    formControlPost.get('userId').setValue('ishak mohamed');
+    expect(formControlPost.get('userId').valid).toEqual(true);
+  });
+
+  it('title is required', () => {
+    const  formControlPost = component.postGroupDetails;
+    expect(formControlPost.get('title').valid).toEqual(false);
+    formControlPost.get('title').setValue('title test');
+    expect(formControlPost.get('title').valid).toEqual(true);
+  });
+
+  it('test title minLength = 5 ', () => {
+    const  formControlPost = component.postGroupDetails;
+    expect(formControlPost.get('title').valid).toEqual(false);
+    formControlPost.get('title').setValue('ishak mohamed');
+    expect(formControlPost.get('title').valid).toEqual(true);
+  });
+
 });
